7b6d5cb1c323394cb1cad5690ebfc914b9e9c039
Rodrigo <rodrigoemmanoel98@gmail.com>
2024-09-27 00:03:07 -0300

    Feat: Adicionado react-router-dom, para deixar o projeto mais proximo o possivel de um real, podendo adicionar varias outras rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Counter } from 'components/Counter'
+import './assets/styles/global.scss'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from 'pages/dashboard'
+import LeftMenu from 'components/leftMenu'
+import Box from '@mui/material/Box'
 
 function App() {
-  return <Counter />
+  return (
+    <Box className="global">
+      <LeftMenu />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Box>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
+  <Router>
     <App />
-  </StrictMode>
+  </Router>
 )
